da90fac071495dfd49a5fec1b9030b149fe41e38
Michael Tichopad <michael@tichopad.dev>
2026-06-06 22:57:49 +0200

    refactor: move button instantiation to the main module

--- extension.js
+++ extension.js
@@ -3,6 +3,7 @@ import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 import { InhibitorManager } from "./inhibitor-manager.js";
 import { NoSleepIndicator } from "./no-sleep-indicator.js";
+import { NoSleepToggle } from "./no-sleep-toggle.js";
 
 /** Main extension entry. */
 export default class NoSleepExtension extends Extension {
@@ -12,6 +13,8 @@ export default class NoSleepExtension extends Extension {
 	#inhibitorManager = null;
 	/** @type {InstanceType<typeof NoSleepIndicator> | null} */
 	#indicator = null;
+	/** @type {InstanceType<typeof NoSleepToggle> | null} */
+	#toggle = null;
 
 	/** Start extension. */
 	enable() {
@@ -20,14 +23,23 @@ export default class NoSleepExtension extends Extension {
 
 		this.#inhibitorManager = new InhibitorManager();
 		this.#indicator = new NoSleepIndicator();
+		this.#toggle = new NoSleepToggle();
 
-		this.#indicator.setup(this.#settings, this.#inhibitorManager);
+		this.#indicator.setup(this.#settings);
+		this.#toggle.setup(this.#settings, this.#inhibitorManager);
+
+		this.#indicator.quickSettingsItems.push(this.#toggle);
 
 		Main.panel.statusArea.quickSettings.addExternalIndicator(this.#indicator);
 	}
 
 	/** Stop extension. */
 	disable() {
+		if (this.#toggle) {
+			this.#toggle.destroy();
+			this.#toggle = null;
+		}
+
 		if (this.#indicator) {
 			this.#indicator.destroy();
 			this.#indicator = null;
--- no-sleep-indicator.js
+++ no-sleep-indicator.js
@@ -3,10 +3,8 @@ import Gio from "gi://Gio";
 import GObject from "gi://GObject";
 import * as QuickSettings from "resource:///org/gnome/shell/ui/quickSettings.js";
 import { ICONS } from "./config.js";
-import { InhibitorManager } from "./inhibitor-manager.js";
-import { NoSleepToggle } from "./no-sleep-toggle.js";
 
-/** Top panel icon. Wires button into the quick menu as well. */
+/** Top panel icon. */
 class _NoSleepIndicator extends QuickSettings.SystemIndicator {
 	/** @type {ReturnType<typeof this._addIndicator> | null} */
 	#indicator = null;
@@ -17,11 +15,10 @@ class _NoSleepIndicator extends QuickSettings.SystemIndicator {
 	}
 
 	/**
-	 * Setup settings and toggle.
+	 * Setup settings.
 	 * @param {Gio.Settings} settings
-	 * @param {InstanceType<typeof InhibitorManager>} inhibitorManager
 	 */
-	setup(settings, inhibitorManager) {
+	setup(settings) {
 		this.#indicator = this._addIndicator();
 		this.#indicator.icon_name = ICONS.on.name;
 		this.#indicator.visible = false;
@@ -36,18 +33,10 @@ class _NoSleepIndicator extends QuickSettings.SystemIndicator {
 			"visible",
 			Gio.SettingsBindFlags.DEFAULT,
 		);
-
-		const toggle = new NoSleepToggle();
-		toggle.setup(settings, inhibitorManager);
-
-		this.quickSettingsItems.push(toggle);
 	}
 
 	/** Cleanup indicator. */
 	destroy() {
-		for (const item of this.quickSettingsItems) {
-			item.destroy();
-		}
 		super.destroy();
 	}
 }
